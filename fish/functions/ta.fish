e7c719cd6dcc10a00487de5e083bc74d3f384fe1
pieps <pieps@google.com>
2020-07-11 15:11:36 -0500

    Updating tmux-management functions.
    They're now idempotent, and have consistent effects regardless of
    whether they're executed from within or without tmux.

--- fish/functions/ta.fish
+++ fish/functions/ta.fish
@@ -1,3 +1,11 @@
 function ta -d 'Attach to tmux session'
-  tmux a $argv
+  if test (count $argv) -gt 0
+    set tmux_session '-t'(basename $argv)
+  end
+
+  if not set -q TMUX
+    tmux a $tmux_session
+  else if set -q tmux_session
+    tmux switchc $tmux_session
+  end
 end
