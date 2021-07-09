b8550c69100c8ad18e94eb46ec901c1e86ee9f5c
Zoe Piepmeier <zoe@pieps.dev>
2021-07-09 01:08:30 -0500

    Switching over to lua config for neovim.
    This uh... still needs a lot of work. LSP config is still not right.

---
+++ neovim/lua/neo/plugins.lua
@@ -0,0 +1,53 @@
+local pack_path =  '/site/pack/packer/start/packer.nvim'
+local install_path = vim.fn.stdpath('data') .. pack_path
+local packer_repo = 'https://github.com/wbthomason/packer.nvim'
+
+if vim.fn.empty(vim.fn.glob(install_path)) > 0 then
+  vim.cmd(table.concat({'!git', 'clone', packer_repo, install_path}, ' '))
+end
+
+vim.api.nvim_exec([[
+  augroup Packer
+    autocmd!
+    autocmd BufWritePost plugins.lua PackerCompile
+  augroup end
+]], false)
+
+require('packer').startup(function(use)
+  use {'nvim-treesitter/nvim-treesitter', run=':TSUpdate'}
+  use 'nvim-treesitter/nvim-treesitter-textobjects'
+  use 'nvim-treesitter/playground'
+
+  -- LSP & languages.
+  use 'neovim/nvim-lspconfig'
+  use 'hrsh7th/nvim-compe'
+  use 'rust-lang/rust.vim'
+  use 'ericcurtin/CurtineIncSw.vim'
+  use 'leafgarland/typescript-vim'
+
+  -- Buffer management.
+  use 'tpope/vim-vinegar'
+  use 'troydm/zoomwintab.vim'
+  use {'junegunn/fzf', run='./install --all'}
+  use 'junegunn/fzf.vim'
+  use 'lukas-reineke/indent-blankline.nvim'
+
+
+  -- Eye candy
+  use 'vim-airline/vim-airline'
+  use {
+    'vim-airline/vim-airline-themes',
+    requires = {'vim-airline/vim-airline'}}
+  use 'powerline/fonts'
+  use 'sainnhe/sonokai'
+  use 'sainnhe/edge'
+  use 'arzg/vim-colors-xcode'
+  use 'sickill/vim-monokai'
+  use 'Mcmartelle/vim-monokai-bold'
+  use 'ludovicchabant/vim-lawrencium'
+  use 'flazz/vim-colorschemes'
+  use 'ryanoasis/vim-devicons'
+  use 'liuchengxu/vista.vim'
+end)
+
+
