4fe06496eaca4850b891a3797ba50153f81ff564
pieps <pieps@google.com>
2022-01-06 16:04:04 -0600

    Untracking fish_{,right_}prompt.fish.
    These are now managed by ilancosman/tide.

--- fish/functions/fish_right_prompt.fish
+++
@@ -1,7 +0,0 @@
-function fish_right_prompt
-  set -l citc_dir (string match -r "\/google\/src\/cloud\/$USER/(\w+)\/" $PWD)
-  set_color yellow
-  if not set -q $citc_dir[2]
-    echo "[$citc_dir[2]]"
-  end
-end
