6ca0f6ca87702041824f005ddd9e38de97dafd58
pieps <zoe@pieps.dev>
2023-09-11 17:21:06 -0500

    Enabling Wayland mode in Firefox when under Wayland.

--- fish/config.fish
+++ fish/config.fish
@@ -4,3 +4,7 @@ if functions -q source_google_fish_package
   #  source_google_fish_package hi
   source_google_fish_package buildfix
 end
+
+if [ "$XDG_SESSION_TYPE" = "wayland" ]
+  export MOZ_ENABLE_WAYLAND=1
+end
