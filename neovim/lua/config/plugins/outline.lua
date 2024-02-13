5951084243cc15d5d173f51d749f1595fd92b843
pieps <pieps@google.com>
2024-02-13 13:59:02 -0600

    Replace vista.vim with outline.nvim.
    Also remove fidget.nvim. I never actually notice the fidgetybit in the
    corner.

---
+++ neovim/lua/config/plugins/outline.lua
@@ -0,0 +1,16 @@
+local util = require('config.util')
+
+local M = {
+	util.gh('hedyhli/outline.nvim'),
+	config = function()
+		-- Example mapping to toggle outline
+		vim.keymap.set("n", "<leader>o", "<cmd>Outline<CR>",
+			{ desc = "Toggle Outline" })
+
+		require("outline").setup {
+			-- Your setup opts here (leave empty to use defaults)
+		}
+	end,
+}
+
+return M
