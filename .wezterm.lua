7fc4421bd4d72413d790d65895b9cc22abe50d6d
Zoe Piepmeier <pieps@kanpachi.local>
2026-04-03 14:13:43 -0500

    Adding wezterm config.

---
+++ .wezterm.lua
@@ -0,0 +1,11 @@
+local wezterm = require 'wezterm'
+
+local config = wezterm.config_builder()
+
+config.font = wezterm.font 'FiraCode Nerd Font'
+config.color_scheme = 'Sonokai (Gogh)'
+config.window_background_opacity = 0.8
+config.text_background_opacity = 0.8
+config.hide_tab_bar_if_only_one_tab = true
+
+return config
