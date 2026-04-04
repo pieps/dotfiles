c573e70ecba9c48a39194a2f030d40f239617e2b
Zoe Piepmeier <zoe@pieps.dev>
2026-04-04 17:49:40 -0500

    Set $TERM to 'wezterm'

--- .wezterm.lua
+++ .wezterm.lua
@@ -7,5 +7,6 @@ config.color_scheme = 'Sonokai (Gogh)'
 config.window_background_opacity = 0.8
 config.text_background_opacity = 0.8
 config.hide_tab_bar_if_only_one_tab = true
+config.term = 'wezterm'
 
 return config
